0e08029d29ff9cc99575cde0f3fb749fc26fca90
kaloniarvind827-svg <kaloniarvind827@gmail.com>
2025-10-18 22:14:42 +0530

    Added the user input program

---
+++ Basics/Userinput.py.py
@@ -0,0 +1,4 @@
+user_name = input("Enter your name : ")
+user_age = int(input("Enter your age : "))
+
+print(f'Hello {user_name}! How are you? You will be  {user_age+1} year old next year')
--- Basics/user_input.py
+++
@@ -1,4 +0,0 @@
-user_name = input("Enter your name : ")
-user_age = int(input("Enter your age : "))
-
-print(f'Hello,{user_name},Next year,you will be {user_age} years old')
